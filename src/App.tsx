0dba3fde52a0903b29ebc3831b842d044b21b6ce
Efraim Nabil <efraimefefraim@gmail.com>
2024-05-03 02:57:45 +0300

    add the knight and it start with random position

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { Chessboard } from 'react-chessboard'
 import './App.css'
+import { BoardPosition } from 'react-chessboard/dist/chessboard/types'
+import { useState } from 'react'
 
 function App() {
 
+  // Random position for the knight
+  const randomPosition = (): BoardPosition => {
+    const row = Math.floor(Math.random() * 8)
+    const col = Math.floor(Math.random() * 8)
+    const square = String.fromCharCode('a'.charCodeAt(0) + row) + (col + 1)
+    const position: BoardPosition = {}
+    position[square as keyof BoardPosition] = 'wN'
+    return position
+}
+
+const [position, setPosition] = useState< BoardPosition | undefined>(randomPosition())
+
   return (
     <div>
-      <Chessboard 
-          position="start"
+      <Chessboard
+          position={position}
           arePiecesDraggable={false}
           boardWidth={600}
       />
